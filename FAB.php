53c40c7491664a9c80b591280342f263f8d7ee6f
Albert Gukasian <admin@algsupport.com>
2022-11-07 17:54:51 +0200

    Added brands

---
+++ FAB.php
@@ -0,0 +1,12 @@
+<?php
+
+	namespace algsupport\yii\fontawesome;
+
+    class FAB extends FontAwesome
+	{
+		/**
+		 * CSS class prefix
+		 * @var string
+		 */
+		public static string $cssPrefix = 'fa-brands';
+	}
